800e711d3bf1ac118d919b94ed8f266e80fbf9e1
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-08-28 22:53:52 -0500

    still work in progress

--- sandwich.js
+++ sandwich.js
@@ -1,23 +1,61 @@
 $(document).ready(function(){
+//below works
+	$("#sandButton").click(function(){
+		// console.log(alert("your button works"));
 
-$("#sandButton").click(function(){
-	console.log(alert("your button works"));
+		$('input[type=checkbox]').each(function () {
+           var sandIngred;
+           var strSandIng =[];
+			// for (var i=0; i<sandIngred.length; i++) {
+
+          		if (this.checked) {
+              	sandIngred= [($(this).val())];
+              	console.log(sandIngred);
+
+              	
+              	
+              	
+              	for (var i =0; i<sandIngred.length; i++) {
+              		strSandIng.push('<span' + sandIngred[i] + '</span>');
+              	}
+              	
+
+
+//this works test below
+               // console.log($(this).val()); 
+           		}
 	
-if($("#meat:checked").val() === "ham") {
-	console.log("it's pork time");
-	$("#custOrder").html("Ham");
-
-} else {
-	alert("no pork for you");
-}
-
-// var $selectedMeat = $("#meat:checked").forEach(value);
-// console.log($selectedMeat);
-
-// var $testVar = $("input:checked").forEach("#meat");
-// console.log($testVar);
-
+		});	
 	});
-
 });
 
+
+
+
+
+
+
+
+
+	// for(var i=0; i<meatArray.length; i++) {
+	//  if($("#meat:checked").val() === meatArray[i]) {
+	//  	console.log(meatArray[i]);
+
+	 	// $("#custOrder").html()f
+// 	 }
+// }
+	
+		// if($("#meat:checked").val() === "ham") {
+		// console.log("it's pork time");
+		// $("#custOrder").html("Ham");
+		// }
+
+		// if($("#meat:checked").val() === "turkey") {
+		// console.log("it's turkey time");
+		// $("#custOrder").html("Turkey");
+		// }
+	//  else {
+	// alert("no pork for you");
+	// }
+// });
+//
